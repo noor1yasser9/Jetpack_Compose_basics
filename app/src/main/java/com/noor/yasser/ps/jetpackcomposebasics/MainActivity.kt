f70ac3f7ee0a152b1915518a8a9b9e2ecede6b76
OMEN <mrh61037@gmail.com>
2021-10-28 11:41:37 +0300

    column

--- app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/MainActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -53,17 +53,17 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
 
 @Composable
 private fun MyApp() {
-    val shouldShowOnboarding = remember { mutableStateOf(true) }
-    if (shouldShowOnboarding.value)
-        OnboardingScreen(onContinueClicked = { shouldShowOnboarding.value = false })
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(value = true) }
+    if (shouldShowOnboarding)
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
     else
         Greetings()
 }
 
 @Composable
 private fun Greetings(names: List<String> = List(1000) { "$it" }) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        names.forEach {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) {
             Greeting(it)
         }
     }
