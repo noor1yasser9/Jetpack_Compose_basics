81bed0d21b724a58b878a77cf5ce9ec9844a2e1c
OMEN <mrh61037@gmail.com>
2021-10-29 23:26:43 +0300

    create your custom layout

--- app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/layouts_in_jetpack_compose_2/MainActivity.kt
+++ app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/layouts_in_jetpack_compose_2/MainActivity.kt
@@ -20,8 +20,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.noor.yasser.ps.jetpackcomposebasics.layouts_in_jetpack_compose_2.ui.theme.LayoutsCodelabTheme
@@ -145,13 +150,13 @@ fun LayoutsCodelab() {
                     .padding(innerPadding)
                     .padding(8.dp)
             )
-            LazyList(scrollState)
+//            LazyList(scrollState)
         }
     }
 }
 
 @Composable
-fun LazyList(scrollState:LazyListState) {
+fun LazyList(scrollState: LazyListState) {
 
     LazyColumn(state = scrollState) {
         items(100) {
@@ -162,9 +167,15 @@ fun LazyList(scrollState:LazyListState) {
 
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(8.dp)) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layouts codelab")
+//    Column(modifier = modifier.padding(8.dp)) {
+//        Text(text = "Hi there!")
+//        Text(text = "Thanks for going through the Layouts codelab")
+//    }
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
     }
 }
 
@@ -185,6 +196,59 @@ fun ImageListItem(index: Int) {
     }
 }
 
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    })
+
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    LayoutsCodelabTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutsCodelabTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
+    }
+}
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    // custom layout attributes
+    content: @Composable () -> Unit
+) {
+    Layout(content = content, modifier = modifier) { measurables, constraints ->
+        // measure and position children given constraints logic here
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints = constraints)
+        }
+        var yPosition = 0
+        layout(constraints.maxWidth, constraints.minHeight) {
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
 @Preview
 @Composable
 fun PhotographerCardPreview() {
@@ -192,6 +256,7 @@ fun PhotographerCardPreview() {
         PhotographerCard()
     }
 }
+
 
 @Preview
 @Composable
