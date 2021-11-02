32965eb7684ee0eafd03a0952efb453224f137cb
OMEN <mrh61037@gmail.com>
2021-11-02 10:09:43 +0200

    create StaggeredGrid

--- app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/complex_custom_layout/MainActivity.kt
+++ app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/complex_custom_layout/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
 import com.noor.yasser.ps.jetpackcomposebasics.jetpack_compose_basics_1.ui.theme.BasicsCodelabTheme
-import java.lang.reflect.Modifier
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,10 +25,47 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun StaggeredGrid(
-    modifier: Modifier = Modifier(),
-    row: Int = 3,
+    modifier: Modifier = Modifier,
+    rowas: Int = 3,
     content: @Composable () -> Unit
 ) {
+    Layout(content = content, modifier = modifier) { measurables, constraints ->
+
+        val rowWidth = IntArray(rowas) { 0 }
+        val rowHeights = IntArray(rowas) { 0 }
+
+        val placeables = measurables.mapIndexed { index, measurable ->
+
+            val placeable = measurable.measure(constraints = constraints)
+            val row = index % rowas
+            rowWidth[row] += placeable.width
+            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+            placeable
+        }
+        val width = rowWidth.maxOrNull()
+            ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+        val height = rowHeights.sumOf { it }
+            .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rowas) { 0 }
+        for (i in 1 until rowas) {
+            rowY[i] = rowY[i - 1] + rowHeights[i - 1]
+        }
+
+        layout(width, height) {
+            val rowX = IntArray(rowas) { 0 }
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rowas
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+
+
+    }
 
 }
 
