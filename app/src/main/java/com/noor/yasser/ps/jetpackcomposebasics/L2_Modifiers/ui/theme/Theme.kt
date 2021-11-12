313c7d7fc507ff9c1e747b1a0a756c4990b69f63
OMEN <mrh61037@gmail.com>
2021-11-12 18:06:32 +0200

    explain Text and Text Style - color - font size - font family - font weight - text align - text decoration text buildAnnotatedString - withStyle   - append - append

--- app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L2_Modifiers/ui/theme/Theme.kt
+++ app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L2_Modifiers/ui/theme/Theme.kt
@@ -8,20 +8,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    secondary = com.noor.yasser.ps.jetpackcomposebasics.L3_Image_Card_Composable.ui.theme.Teal200,
-    surface = com.noor.yasser.ps.jetpackcomposebasics.L3_Image_Card_Composable.ui.theme.Blue,
-    onSurface = com.noor.yasser.ps.jetpackcomposebasics.L3_Image_Card_Composable.ui.theme.Navy,
-    primary = com.noor.yasser.ps.jetpackcomposebasics.L3_Image_Card_Composable.ui.theme.Navy,
-    onPrimary = com.noor.yasser.ps.jetpackcomposebasics.L3_Image_Card_Composable.ui.theme.Chartreuse
+    secondary = com.noor.yasser.ps.jetpackcomposebasics.L4_Styling_Text.ui.theme.Teal200,
+    surface = com.noor.yasser.ps.jetpackcomposebasics.L4_Styling_Text.ui.theme.Blue,
+    onSurface = com.noor.yasser.ps.jetpackcomposebasics.L4_Styling_Text.ui.theme.Navy,
+    primary = com.noor.yasser.ps.jetpackcomposebasics.L4_Styling_Text.ui.theme.Navy,
+    onPrimary = com.noor.yasser.ps.jetpackcomposebasics.L4_Styling_Text.ui.theme.Chartreuse
 )
 
 private val LightColorPalette = lightColors(
-    primaryVariant = com.noor.yasser.ps.jetpackcomposebasics.L3_Image_Card_Composable.ui.theme.Navy,
-    secondary = com.noor.yasser.ps.jetpackcomposebasics.L3_Image_Card_Composable.ui.theme.Teal200,
-    surface = com.noor.yasser.ps.jetpackcomposebasics.L3_Image_Card_Composable.ui.theme.Blue,
+    primaryVariant = com.noor.yasser.ps.jetpackcomposebasics.L4_Styling_Text.ui.theme.Navy,
+    secondary = com.noor.yasser.ps.jetpackcomposebasics.L4_Styling_Text.ui.theme.Teal200,
+    surface = com.noor.yasser.ps.jetpackcomposebasics.L4_Styling_Text.ui.theme.Blue,
     onSurface = Color.White,
-    primary = com.noor.yasser.ps.jetpackcomposebasics.L3_Image_Card_Composable.ui.theme.LightBlue,
-    onPrimary = com.noor.yasser.ps.jetpackcomposebasics.L3_Image_Card_Composable.ui.theme.Navy
+    primary = com.noor.yasser.ps.jetpackcomposebasics.L4_Styling_Text.ui.theme.LightBlue,
+    onPrimary = com.noor.yasser.ps.jetpackcomposebasics.L4_Styling_Text.ui.theme.Navy
     /* Other default colors to override
     background = Color.White,
     surface = Color.White,
---
+++ app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L4_Styling_Text/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.noor.yasser.ps.jetpackcomposebasics.L4_Styling_Text
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.noor.yasser.ps.jetpackcomposebasics.R
+import com.noor.yasser.ps.jetpackcomposebasics.complex_custom_layout.padding
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val fontFamily = FontFamily(
+            Font(R.font.dubaimedium, FontWeight.Thin),
+            Font(R.font.kalery, FontWeight.Bold),
+            Font(R.font.yaseer, FontWeight.Medium),
+        )
+        setContent {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xff101010))
+            ) {
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ){
+                            append("J")
+                        }
+                        append("etpack ")
+                        withStyle(
+                            style = SpanStyle(
+                                Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ){
+                            append("C")
+                        }
+                        append("ompose ")
+                    },
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.LineThrough
+                )
+            }
+        }
+    }
+}
+
+
+
+
+
