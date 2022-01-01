4b5b9174ef6848dace196458f6b0c4c5e84a06f1
OMEN <mrh61037@gmail.com>
2022-01-01 19:11:43 +0200

    add font for facebook icon draw facebookIcon draw MessengerIcon draw GoogleIcon draw YoutubeIcon draw GooglePhotosIcon

--- app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L15_jetpackcompose_canvas_icon_pack/MainActivity.kt
+++ app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L15_jetpackcompose_canvas_icon_pack/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.noor.yasser.ps.jetpackcomposebasics.L15_jetpackcompose_canvas_icon_pack
 
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.Typeface
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,10 +13,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -37,8 +41,14 @@ fun getAllIcons() {
     ) {
         Row(horizontalArrangement = Arrangement.SpaceAround) {
             InstagramIcon()
-//            facebookIcon()
-//            messengerIcon()
+            FacebookIcon()
+            MessengerIcon()
+        }
+
+        Row(horizontalArrangement = Arrangement.SpaceAround) {
+            GoogleIcon()
+            YoutubeIcon()
+            GooglePhotosIcon()
         }
     }
 }
@@ -69,5 +79,207 @@ fun InstagramIcon() {
         )
     }
 
+}
 
+@Composable
+fun FacebookIcon() {
+    val assetManager = LocalContext.current.assets
+    val paint = Paint().apply {
+        textAlign = Paint.Align.CENTER
+        textSize = 200f
+        color = Color.White.toArgb()
+        typeface = Typeface.createFromAsset(assetManager, "FACEBOLF.OTF")
+    }
+
+    Canvas(
+        modifier = Modifier
+            .size(100.dp)
+            .padding(16.dp)
+    ) {
+        drawRoundRect(
+            color = Color(0xFF1776d1),
+            cornerRadius = CornerRadius(20f, 20f)
+        )
+        drawContext.canvas.nativeCanvas.drawText(
+            "f",
+            center.x + 25,
+            center.y + 90, paint
+        )
+
+    }
+}
+
+@Composable
+fun MessengerIcon() {
+    val colors = listOf(Color(0xFF02b8f9), Color(0xFF0277fe))
+    Canvas(
+        modifier = Modifier
+            .size(100.dp)
+            .padding(16.dp)
+    ) {
+        val trianglePath = androidx.compose.ui.graphics.Path().let {
+            it.moveTo(this.size.width * .20f, this.size.height * .77f)
+            it.lineTo(this.size.width * .20f, this.size.height * 0.95f)
+            it.lineTo(this.size.width * .37f, this.size.height * 0.86f)
+            it.close()
+            it
+        }
+
+
+        val electricPath = androidx.compose.ui.graphics.Path().let {
+            it.moveTo(this.size.width * .20f, this.size.height * 0.60f)
+            it.lineTo(this.size.width * .45f, this.size.height * 0.35f)
+            it.lineTo(this.size.width * 0.56f, this.size.height * 0.46f)
+            it.lineTo(this.size.width * 0.78f, this.size.height * 0.35f)
+            it.lineTo(this.size.width * 0.54f, this.size.height * 0.60f)
+            it.lineTo(this.size.width * 0.43f, this.size.height * 0.45f)
+            it.close()
+            it
+        }
+        drawOval(
+            Brush.verticalGradient(colors = colors),
+            size = Size(this.size.width, this.size.height * 0.95f)
+        )
+        drawPath(
+            path = trianglePath,
+            Brush.verticalGradient(colors = colors),
+            style = Stroke(width = 15f, cap = StrokeCap.Round)
+        )
+        drawPath(path = electricPath, color = Color.White)
+    }
+}
+
+
+@Composable
+fun GoogleIcon() {
+    Canvas(
+        modifier = Modifier
+            .size(100.dp)
+            .padding(20.dp)
+    ) {
+        val width = this.size.width
+        val height = this.size.height
+        val path = androidx.compose.ui.graphics.Path()
+        path.moveTo(width / 2, height / 2)
+        path.lineTo(width + 5, height * .05f)
+        path.lineTo(width + 20, height * 0.45f)
+
+        clipPath(path = path, clipOp = ClipOp.Difference) {
+            drawCircle(
+                color = Color.Transparent,
+                radius = 100f,
+                style = Stroke(40f, cap = StrokeCap.Round)
+            )
+            drawPath(path = path, color = Color.Transparent)
+        }
+
+        drawRect(
+            color = Color(0xFF4384f3),
+            size = Size(width * .56f, 20f),
+            topLeft = Offset(width * .55f, height * .45f)
+        )
+        drawRect(
+            color = Color(0xFF4384f3),
+            size = Size(width * .45f, 20f),
+            topLeft = Offset(width * .55f, height * .54f)
+        )
+        drawArc(
+            color = Color(0xFF4384f3),
+            startAngle = 0f,
+            sweepAngle = 45f,
+            useCenter = false,
+            style = Stroke(width = 40f)
+        )
+        drawArc(
+            color = Color(0xFF33a852),
+            startAngle = 45f,
+            sweepAngle = 135f,
+            useCenter = false,
+            style = Stroke(width = 40f)
+        )
+
+        drawArc(
+            color = Color(0xFFfabd03),
+            sweepAngle = 80f,
+            startAngle = 145f,
+            useCenter = false,
+            style = Stroke(width = 40f)
+        )
+
+        drawArc(
+            color = Color(0xFFeb4435),
+            startAngle = 205f,
+            sweepAngle = 120f,
+            useCenter = false,
+            style = Stroke(width = 40f)
+        )
+    }
+}
+
+@Composable
+fun YoutubeIcon() {
+    Canvas(
+        modifier = Modifier
+            .size(100.dp)
+            .padding(16.dp)
+    ) {
+        val path = androidx.compose.ui.graphics.Path().apply {
+            moveTo(size.width * .43f, size.height * .38f)
+            lineTo(size.width * .72f, size.height * .55f)
+            lineTo(size.width * .43f, size.height * .73f)
+            close()
+        }
+        drawRoundRect(
+            color = Color.Red,
+            cornerRadius = CornerRadius(40f, 40f),
+            size = Size(size.width, size.height * .70f),
+            topLeft = Offset(size.width.times(.0f), size.height.times(.20f))
+        )
+        drawPath(color = Color.White, path = path)
+    }
+}
+
+
+@Composable
+private fun GooglePhotosIcon() {
+    Canvas(
+        modifier = Modifier
+            .size(100.dp)
+            .padding(16.dp)
+    ) {
+
+        drawArc(
+            color = Color(0xFFf04231),
+            startAngle = -90f,
+            sweepAngle = 180f,
+            useCenter = true,
+            size = Size(size.width * .50f, size.height * .50f),
+            topLeft = Offset(size.width * .25f, 0f)
+        )
+        drawArc(
+            color = Color(0xFF4385f7),
+            startAngle = 0f,
+            sweepAngle = 180f,
+            useCenter = true,
+            size = Size(size.width * .50f, size.height * .50f),
+            topLeft = Offset(size.width * .50f, size.height * .25f)
+        )
+        drawArc(
+            color = Color(0xFF30a952),
+            startAngle = 0f,
+            sweepAngle = -180f,
+            useCenter = true,
+            size = Size(size.width * .50f, size.height * .50f),
+            topLeft = Offset(0f, size.height * .25f)
+        )
+
+        drawArc(
+            color = Color(0xFFffbf00),
+            startAngle = 270f,
+            sweepAngle = -180f,
+            useCenter = true,
+            size = Size(size.width * .50f, size.height * .50f),
+            topLeft = Offset(size.width * .25f, size.height * .50f)
+        )
+    }
 }
