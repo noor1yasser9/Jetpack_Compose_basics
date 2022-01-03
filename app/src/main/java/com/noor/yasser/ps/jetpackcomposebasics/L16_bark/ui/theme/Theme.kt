92817b02dcba49d8fa3e1e18166a453392417790
OMEN <mrh61037@gmail.com>
2022-01-03 18:25:36 +0200

    new branch and init project

---
+++ app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L16_bark/ui/theme/Theme.kt
@@ -0,0 +1,52 @@
+package com.noor.yasser.ps.jetpackcomposebasics.L16_bark.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    secondary = Teal200,
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
+)
+
+private val LightColorPalette = lightColors(
+    primaryVariant = Navy,
+    secondary = Teal200,
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun BasicsCodelabTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography =   Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
