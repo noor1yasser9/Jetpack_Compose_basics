d4da5a33fb3a8b8a0cc4e6dfdb3c14f3df9712fa
OMEN <mrh61037@gmail.com>
2022-01-04 17:37:19 +0200

    finish design TapBar

--- app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L16_bark/view/Home.kt
+++ app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L16_bark/view/Home.kt
@@ -1,2 +1,29 @@
 package com.noor.yasser.ps.jetpackcomposebasics.L16_bark.view
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.noor.yasser.ps.jetpackcomposebasics.L16_bark.model.Dog
+
+@Composable
+fun Home(
+    navController: NavHostController,
+    dogList: List<Dog>, toggleTheme: () -> Unit
+) {
+    LazyColumn {
+        item {
+            Spacer(
+                modifier = Modifier.height(8.dp)
+            )
+        }
+
+        items(dogList) {
+
+        }
+    }
+}
