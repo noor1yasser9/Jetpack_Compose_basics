3f2748b01771e065a081a0fbcf174d4eae5bb56c
OMEN <mrh61037@gmail.com>
2022-01-04 17:43:19 +0200

    finish home Screen design

--- app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L16_bark/view/Home.kt
+++ app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L16_bark/view/Home.kt
@@ -8,6 +8,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.noor.yasser.ps.jetpackcomposebasics.L16_bark.component.ItemDogCard
+import com.noor.yasser.ps.jetpackcomposebasics.L16_bark.component.TopBar
 import com.noor.yasser.ps.jetpackcomposebasics.L16_bark.model.Dog
 
 @Composable
@@ -17,13 +19,24 @@ fun Home(
 ) {
     LazyColumn {
         item {
+            TopBar(onToggle = toggleTheme)
             Spacer(
                 modifier = Modifier.height(8.dp)
             )
+
         }
 
-        items(dogList) {
-
+        items(dogList) { item ->
+            ItemDogCard(dog = item,
+                onItemClicked = { dog ->
+                    navController.navigate(
+                        "details/${
+                            dog.id
+                        }/${dog.name}/${
+                            dog.location
+                        }"
+                    )
+                })
         }
     }
 }
