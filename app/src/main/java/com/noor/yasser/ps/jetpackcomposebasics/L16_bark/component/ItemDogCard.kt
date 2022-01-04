4252415fd1ab691f42d1d62f9161a8f0f36a6986
OMEN <mrh61037@gmail.com>
2022-01-04 19:57:59 +0200

    finished design for ItemDogCard.kt for Home Screen

--- app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L16_bark/component/ItemDogCard.kt
+++ app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L16_bark/component/ItemDogCard.kt
@@ -26,6 +26,8 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.noor.yasser.ps.jetpackcomposebasics.L16_bark.model.Dog
 import com.noor.yasser.ps.jetpackcomposebasics.R
@@ -41,6 +43,67 @@ fun ItemDogCard(dog: Dog, onItemClicked: (dog: Dog) -> Unit) {
         elevation = 0.dp,
         backgroundColor = MaterialTheme.colors.onSurface
     ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            val image: Painter = painterResource(id = dog.image)
+            Image(
+                modifier = Modifier
+                    .size(80.dp, 80.dp)
+                    .clip(RoundedCornerShape(16.dp)),
+                painter = image,
+                alignment = Alignment.CenterStart,
+                contentDescription = "",
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column(modifier = Modifier.align(Alignment.CenterVertically)) {
+                Text(
+                    text = dog.name,
+                    modifier = Modifier.padding(0.dp, 0.dp, 12.dp, 0.dp),
+                    color = MaterialTheme.colors.surface,
+                    fontWeight = FontWeight.Bold,
+                    style = typography.subtitle1
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = buildString {
+                        append(dog.age)
+                        append("yrs | ")
+                        append(dog.gender)
+                    },
+                    modifier = Modifier.padding(0.dp, 0.dp, 12.dp, 0.dp),
+                    color = MaterialTheme.colors.surface,
+                    style = typography.caption
+                )
 
+                Row(verticalAlignment = Alignment.Bottom) {
+
+                    val location: Painter = painterResource(id = R.drawable.ic_location)
+
+                    Icon(
+                        painter = location,
+                        contentDescription = null,
+                        modifier = Modifier.size(16.dp, 16.dp),
+                        tint = Color.Red
+                    )
+                    Text(
+                        text = dog.location,
+                        modifier = Modifier.padding(8.dp, 12.dp, 12.dp, 0.dp),
+                        color = MaterialTheme.colors.surface,
+                        style = typography.caption
+                    )
+                }
+
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                GenderTag(dog.gender)
+            }
+        }
     }
 }
