f5116531768b73925b29f7732ed4d596531cccce
OMEN <mrh61037@gmail.com>
2022-01-04 18:06:28 +0200

    add two function first for setDayTheme second for setDarkTheme and display screen WigglesMain update library for navigation and other updated

--- app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L16_bark/view/WigglesMain.kt
+++ app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L16_bark/view/WigglesMain.kt
@@ -1,13 +1,74 @@
 package com.noor.yasser.ps.jetpackcomposebasics.L16_bark.view
 
-import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.*
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
+
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import com.noor.yasser.ps.jetpackcomposebasics.L16_bark.data.FakeDogDatabase
+import com.noor.yasser.ps.jetpackcomposebasics.L16_bark.navigation.Screen
+
 
 @ExperimentalAnimationApi
 @Composable
 fun WigglesMain(toggleTheme: () -> Unit) {
     val navController = rememberAnimatedNavController()
-
+    AnimatedNavHost(
+        navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(
+            Screen.Home.route,
+            exitTransition = { _, _ ->
+                slideOutHorizontally(
+                    targetOffsetX = { -300 },
+                    animationSpec = tween(
+                        durationMillis = 300,
+                        easing = FastOutSlowInEasing
+                    )
+                ) + fadeOut(animationSpec = tween(300))
+            },
+            popEnterTransition = { _, _ ->
+                slideInHorizontally(
+                    initialOffsetX = { -300 },
+                    animationSpec = tween(
+                        durationMillis = 300,
+                        easing = FastOutSlowInEasing
+                    )
+                ) + fadeIn(animationSpec = tween(300))
+            },
+        ) {
+            Home(
+                navController = navController,
+                dogList = FakeDogDatabase.dogList,
+                toggleTheme = toggleTheme
+            )
+        }
+        composable(
+            "${Screen.Details.route}/{id}/{title}/{location}",
+            enterTransition = { _, _ ->
+                slideInHorizontally(
+                    initialOffsetX = { 300 },
+                    animationSpec = tween(
+                        durationMillis = 300,
+                        easing = FastOutSlowInEasing
+                    )
+                ) + fadeIn(animationSpec = tween(300))
+            },
+            exitTransition = { _, _ ->
+                slideOutHorizontally(
+                    targetOffsetX = { 300 },
+                    animationSpec = tween(
+                        durationMillis = 300,
+                        easing = FastOutSlowInEasing
+                    )
+                ) + fadeOut(animationSpec = tween(300))
+            },
+//            arguments = listOf(navArgument("id") { type = NavType.IntType })
+        ) {}
+    }
 
 }
