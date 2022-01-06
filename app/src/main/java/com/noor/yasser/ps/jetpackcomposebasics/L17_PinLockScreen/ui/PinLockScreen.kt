006f43f9f4aaf0a8d76377981cf183a11903a48e
OMEN <mrh61037@gmail.com>
2022-01-06 18:05:09 +0200

    create pinLockScreen and finish screen add success.json add material library

--- app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L17_PinLockScreen/ui/PinLockScreen.kt
+++ app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L17_PinLockScreen/ui/PinLockScreen.kt
@@ -1,21 +1,35 @@
 package com.noor.yasser.ps.jetpackcomposebasics.L17_PinLockScreen.ui
 
+import android.content.Context
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme.typography
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Circle
+import androidx.compose.material.icons.outlined.Backspace
+import androidx.compose.material.icons.outlined.Circle
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.noor.yasser.ps.jetpackcomposebasics.R
 import kotlinx.coroutines.delay
 
@@ -70,9 +84,179 @@ fun PinLockScreen() {
                 )
                 Spacer(modifier = Modifier.height(30.dp))
                 if (showSuccess.value) {
+                    LottieLoadingView(
+                        context = context,
+                        file = "success.json",
+                        iterations = 1,
+                        modifier = Modifier.size(100.dp)
+                    )
+                } else {
+                    Row {
+                        (0 until pinSize).forEach {
+                            Icon(
+                                imageVector = if (inputPin.size > it) Icons.Default.Circle else Icons.Outlined.Circle,
+                                contentDescription = it.toString(),
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .size(30.dp),
+                                tint = Color.Black
+                            )
+                        }
+                    }
+                }
+                Text(
+                    text = error.value,
+                    color = MaterialTheme.colorScheme.error,
+                    modifier = Modifier.padding(16.dp)
+                )
 
+                Spacer(modifier = Modifier.height(50.dp))
+
+            }
+            Column(
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 20.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    (1..3).forEach {
+                        PinKeyItem(
+                            onClick = { inputPin.add(it) }
+                        ) {
+                            Text(
+                                text = it.toString(),
+                                style = typography.h5
+                            )
+                        }
+                    }
+
+                }
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    (4..6).forEach {
+                        PinKeyItem(
+                            onClick = { inputPin.add(it) }
+                        ) {
+                            Text(
+                                text = it.toString(),
+                                style = typography.h5
+                            )
+                        }
+                    }
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    (7..9).forEach {
+                        PinKeyItem(
+                            onClick = { inputPin.add(it) }
+                        ) {
+                            Text(
+                                text = it.toString(),
+                                style = typography.h5,
+                                modifier = Modifier.padding(4.dp)
+                            )
+                        }
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 10.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Check,
+                        contentDescription = "Success",
+                        modifier = Modifier
+                            .size(25.dp)
+                            .clickable { }
+                    )
+                    PinKeyItem(
+                        onClick = { inputPin.add(0) },
+                        modifier = Modifier.padding(horizontal = 16.dp,
+                            vertical = 8.dp)
+                    ) {
+                        Text(
+                            text = "0",
+                            style = typography.h5,
+                            modifier = Modifier.padding(4.dp)
+                        )
+                    }
+                    Icon(
+                        imageVector = Icons.Outlined.Backspace,
+                        contentDescription = "Clear",
+                        modifier = Modifier
+                            .size(25.dp)
+                            .clickable {
+                                if (inputPin.isNotEmpty()) {
+                                    inputPin.removeLast()
+                                }
+                            }
+                    )
                 }
             }
         }
     }
+}
+
+@Composable
+fun LottieLoadingView(
+    context: Context, file: String,
+    modifier: Modifier = Modifier,
+    iterations: Int = 10
+) {
+    val composition by rememberLottieComposition(spec = LottieCompositionSpec.Asset(file))
+    LottieAnimation(
+        composition = composition,
+        modifier = modifier.defaultMinSize(300.dp),
+        iterations = iterations
+    )
+}
+
+@Composable
+fun PinKeyItem(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier.padding(8.dp),
+    shape: Shape = androidx.compose.material.MaterialTheme.shapes.small.copy(CornerSize(percent = 50)),
+    backgroundColor: Color = MaterialTheme.colorScheme.onPrimary,
+    contentColor: Color = contentColorFor(backgroundColor = backgroundColor),
+    elevation: Dp = 4.dp,
+    content: @Composable () -> Unit
+) {
+    Surface(
+        modifier = modifier,
+        shape = shape,
+        color = backgroundColor,
+        contentColor = contentColor,
+        tonalElevation = elevation,
+        onClick = onClick,
+        role = Role.Button,
+        indication = rememberRipple()
+    ) {
+        CompositionLocalProvider(
+            LocalContentAlpha provides contentColor.alpha
+        ) {
+            ProvideTextStyle(
+                androidx.compose.material3.MaterialTheme.typography.displayMedium
+            ) {
+                Box(
+                    modifier = Modifier.defaultMinSize(minWidth = 64.dp, minHeight = 64.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    content()
+                }
+            }
+        }
+    }
+
 }
