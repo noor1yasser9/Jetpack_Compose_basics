e30ecc239fb3a7c687138ab9392be96334061163
OMEN <mrh61037@gmail.com>
2022-01-07 11:16:03 +0200

    create exoplayer using library and finished

---
+++ app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L18_ExoPlayerVideo/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.noor.yasser.ps.jetpackcomposebasics.L18_ExoPlayerVideo
+
+
+import android.annotation.SuppressLint
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import com.noor.yasser.ps.jetpackcomposebasics.L17_PinLockScreen.ui.PinLockScreen
+import com.noor.yasser.ps.jetpackcomposebasics.L17_PinLockScreen.ui.theme.PinLockScreenTheme
+import com.noor.yasser.ps.jetpackcomposebasics.L17_PinLockScreen.ui.theme.Purple500
+import com.noor.yasser.ps.jetpackcomposebasics.L18_ExoPlayerVideo.ui.theme.ExoPlayerVideojetPackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ExoPlayerVideojetPackComposeTheme {
+                ExoPlayer()
+            }
+        }
+    }
+}
+
+
+@SuppressLint("RememberReturnType")
+@Composable
+fun ExoPlayer() {
+    val context = LocalContext.current
+    val url = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4"
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .background(Purple500),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "ExoPlayer Video",
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+        }
+
+        val exoPlayer = remember(context) {
+            SimpleExoPlayer.Builder(context)
+                .build().apply {
+                    val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(
+                        context, Util.getUserAgent(context, context.packageName)
+                    )
+                    val source = ProgressiveMediaSource.Factory(dataSourceFactory)
+                        .createMediaSource(Uri.parse(url))
+
+                    this.prepare(source)
+                }
+        }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                AndroidView(
+                    factory = { context ->
+                        PlayerView(context).apply {
+                            player = exoPlayer
+                        }
+                    }
+                )
+            }
+        }
+
+
+    }
