4bf7a8eaa9672289e9de03e15ce0f03d3c1340d9
OMEN <mrh61037@gmail.com>
2022-01-08 19:57:30 +0200

    new branch update ui theme

--- app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L18_ExoPlayerVideo/ui/theme/Theme.kt
+++ app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/L18_ExoPlayerVideo/ui/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary =  Purple200,
+    primaryVariant =  Purple700,
+    secondary =  Teal200
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primaryVariant =  Purple700,
+    secondary =  Teal200
 
     /* Other default colors to override
     background = Color.White,
@@ -40,7 +40,7 @@ fun ExoPlayerVideojetPackComposeTheme(
 
     MaterialTheme(
         colors = colors,
-        typography = Typography,
+        typography =Typography,
         shapes = Shapes,
         content = content
     )
