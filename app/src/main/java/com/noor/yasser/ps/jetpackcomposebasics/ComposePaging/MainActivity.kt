995305f6726d9e5d9935a60739089168bf506f71
OMEN <mrh61037@gmail.com>
2022-02-14 19:47:15 +0200

    create view model for receive data and create function for load data from next page DefaultPaginator

--- app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/ComposePaging/MainActivity.kt
+++ app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/ComposePaging/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,6 +15,8 @@ import androidx.compose.ui.unit.*
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import androidx.constraintlayout.compose.atLeast
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.noor.yasser.ps.jetpackcomposebasics.ComposePaging.ui.theme.ComposePagingTheme
 import com.noor.yasser.ps.jetpackcomposebasics.jetpack_compose_basics_1.ui.theme.BasicsCodelabTheme
 import com.noor.yasser.ps.jetpackcomposebasics.layouts_in_jetpack_compose_2.ui.theme.LayoutsCodelabTheme
 
@@ -22,7 +25,44 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            ComposePagingTheme {
+                val viewModel = viewModel<MainViewModel>()
+                val state = viewModel.state
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    items(state.items.size) { i ->
+                        val item = state.items[i]
+                        if (i >= state.items.size - 1 && !state.endReached && !state.isLoading) {
+                            viewModel.loadNextItems()
+                        }
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        ) {
+                            Text(
+                                text = item.title,
+                                fontSize = 20.sp,
+                                color = Color.Black
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(item.description)
+                        }
+                    }
 
+                    item {
+                        if (state.isLoading) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(8.dp),
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                CircularProgressIndicator()
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/ComposePaging/ui/theme/Theme.kt
+++ app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/ComposePaging/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun ComposePagingYTTheme(
+fun ComposePagingTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
