f3e01b836a7ba910d592dad0a8f8170176350960
OMEN <mrh61037@gmail.com>
2022-03-04 19:40:25 +0200

    add meta data for map key in manifest update MainActivity and Go to map Screen add model Parking Spot

--- app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/map/MainActivity.kt
+++ app/src/main/java/com/noor/yasser/ps/jetpackcomposebasics/map/MainActivity.kt
@@ -3,6 +3,11 @@ package com.noor.yasser.ps.jetpackcomposebasics.map
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.noor.yasser.ps.jetpackcomposebasics.map.presentation.MapScreen
 import com.noor.yasser.ps.jetpackcomposebasics.map.ui.theme.MapsComposeGuideTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +16,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MapsComposeGuideTheme {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MapScreen()
+                }
             }
         }
     }
